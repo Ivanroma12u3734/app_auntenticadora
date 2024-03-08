c82ce5b10510cd22900fd0515a42e1106ecdfeb1
matriculas <Ivanroma12u3734>
2024-03-08 00:09:52 -0600

    este es

--- routes/index.js
+++ routes/index.js
@@ -16,23 +16,23 @@ router.post('/users/create', (req, res) => {
     users.push(req.body)
     res.send("ok")
 })
-    //est
+//est
 router.get('/users', (req, res) => {
     res.status("200").json(users)
 });
 
-    //put
-    router.put('/users/update/:id',(req,res)=>{
-        const user = users.find(u=>u.id === req.params.id);
-        if (!user){
-            res.status(404).send ("El recurso no se encuentra");
-            return
-        }
-        const updateUser = {...user , ... req.body }
+//put
+router.put('/users/update/:id', (req, res) => {
+    const user = users.find(u => u.id === req.params.id);
+    if (!user) {
+        res.status(404).send("El recurso no se encuentra");
+        return
+    }
+    const updateUser = { ...user, ...req.body }
 
-        users = users.map (u=>u.id === req.params.id ? updateUser : u);
-        res.status(200).send("usuario actualizado con exito ");
+    users = users.map(u => u.id === req.params.id ? updateUser : u);
+    res.status(200).send("usuario actualizado con exito ");
 
-    })
-    
+})
+
 export default router;
